f5f2ca3ab423ebd6ca23a9a028c62d7f32957b2d
Jacob Flohre <jake@heysparkbox.com>
2013-03-05 09:32:27 -0500

    Update fizzbuzz and spec   tests still not working

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,19 +3,26 @@ class Game
 	def play(numbers)
 		numbers.each do |number| 
 			if number%3==0 && number%5==0 
-				puts "fizzbuzz"
+				fizzbuzz
 			elsif number%3==0  
-				puts "fizz"
+				fizz
 			elsif number%5==0 
-				puts "buzz"
+				buzz
 			else
 				puts "#{number}" 
 			end
 		end
 	end
+	def fizz
+		print "fizz\n"
+	end
+	def fizzbuzz
+		print "fizzbuzz\n"
+	end
+	def buzz
+		print "buzz\n"
+	end
 end
 
 #game = Game.new
 #game.play(1..100)
-
-
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,15 +5,19 @@ describe 'fizzbuzz' do
 		game.should be_an_instance_of Game
 	end
 	it 'should return the number if it is not fizz/buzz/fizzbuzz' do
-		game.play(1..2).should include(1)
+		game.play(1..1).should include(1)
 	end
-	it 'should return the fizz if it is fizzable' do
-		game.play(1..5).should_not include (5)
+	it 'should return fizz if it is fizzable' do
+		game.play(3..3).should_not include('fizz')
+	  #game.play(3..3).should_not == game.fizz
+		#game.play(3..3).should_not == 'fizz'		
 	end
 	it 'should return buzz if it is buzzable' do
-		game.play(3).should == 'fizz'
+		game.play(5..5).should_not == 5
 	end
 	it 'should return fizzbuzz if it is fizzable and buzzable' do
-		game.play(15).should == 'fizzbuzz'
+		game.play(15..15).should_not == 15
+		game.play(15..15).should_not == 'fizz'
+		game.play(15..15).should_not == 'buzz'
 	end
 end
